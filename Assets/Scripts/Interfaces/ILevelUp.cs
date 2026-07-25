ad563cf11d9b4fa87edddf12dde4afc36ce51e36
KingSton2846 <maximlitvinov42@mail.ru>
2026-07-25 12:52:47 +0300

    ThirdCommit

---
+++ Assets/Scripts/Interfaces/ILevelUp.cs
@@ -0,0 +1,4 @@
+public interface ILevelUp
+{
+    void LevelUp(CharacterData data, int level);
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,11 @@ public class PlayerController : MonoBehaviour
     private Vector2 _moveInput;
 
     private PlayerInput _playerInput;
-    private CharacterData _characterData;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _playerInput = GetComponent<PlayerInput>();
-        _characterData = GetComponent<CharacterData>();
     }
 
     public void LoadConfig(PlayerConfigSO config)
@@ -41,7 +39,6 @@ public class PlayerController : MonoBehaviour
         if (value.isPressed && IsGrounded())
         {
             _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpForce);
-            _characterData.ScoreUp(5);
         }
     }
 
